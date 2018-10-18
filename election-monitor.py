0af85bf89e7341ddc88d1cb922460d84a4eff4dc
Aaron Dietz <aarondietz@users.noreply.github.com>
2018-10-17 22:51:48 -0700

    First attempt at a repeating timer, not yet working well

--- election-monitor.py
+++ election-monitor.py
@@ -2,6 +2,21 @@
 """
 Created on Sat Sep 15 17:19:10 2018
 
+This is written to help monitor elections.
+
+The idea is to record election result data over time so we can later analyze
+trends to detect inconsistencies that may help us identify tampering, mistakes, 
+or other impacts to an election.
+
+The program will download a copy of an election result web page, at a timed
+frequency. Later, we'll need to write data extraction code to get the data
+into an analyzable format. As a worst case scenario, we could write these for
+the few elections that are the most suspect. Best case scenario is to have 
+enough volunteers to harvest nearly all our results in a mutually sharable
+format, such that we could compare unusual results against the "norm" and/or
+even recognize overall trends, should widescale tampering or other issues 
+exist.
+
 
 """
 
@@ -11,6 +26,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from pytz import timezone
 import os.path
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Add functionality for dialog box GUI
 
@@ -45,6 +61,10 @@ html = requests.get(url).text
 print (html)
 
 
+# Add functionality to get the suffix of the retrieved web page (.html, .htm, .cfm, etc.)
+# and then append that to the end of the saved copy.
+
+
 
 # Get timestamp - all timestamps will be Eastern time for consistency,
 # in case later we want to stitch data from many users together in time
@@ -59,72 +79,56 @@ loc_dt = datetime.now(eastern)
 
 
 
-# Store the web site
-# Got some of this code from: https://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac
 
 # Establish the file name
 file_name = "test" + "-" + loc_dt.strftime(fmt)
+# Prepend the path
+completePath = os.path.join(save_here, file_name + ".html")     
+
 # Get unique page title from url to use as folder name to group all the retrieved copies
 # in a single directory
 
-# Prepend the path
-completePath = os.path.join(save_here, file_name + ".html")         
-# Open a file object for writing
-file_obj = open(completePath, "w")
-# Write the retrieved web page to the file object
-toFile = html
-file_obj.write(toFile)
-# Close the file object
-file_obj.close()
 
+# Function for storing the web site
+# Got some of this code from: https://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac
+def store_copy(page_to_save, path_to_save_to):  
+    # Open a file object for writing
+    file_obj = open(path_to_save_to, "w")
+    # Write the retrieved web page to the file object
+    toFile = page_to_save
+    file_obj.write(toFile)
+    # Close the file object
+    file_obj.close()
+
+
+
+# Function where everything we need to repeat goes.
+def repeat_tasks():
+    # Call the function for saving a copy of the web page
+    store_copy(html, completePath)
 
 
 # Repeat the retrieval according to the frequency set
+# code snippet originally from: https://stackoverflow.com/questions/22715086/scheduling-python-script-to-run-every-hour-accurately
+# link to documentation: https://apscheduler.readthedocs.io/en/latest/ 
+scheduler = BlockingScheduler()
+scheduler.add_job(repeat_tasks, 'interval', minutes = frequency)
+scheduler.start()
 
 
 
 
 
-
-
-# To do:
+# To do list that might not be tracked anywhere else:
 #
 # Add functionality for capturing various different URL suffixes (html, pdf, etc.) and saving as such.
-# Add functionality for each page to be saved in its own folder
+# Add functionality for each page to be saved in its own folder (for when it can handle more than one page)
 
 
 
 
 
 #### IDEAS - PROBABLY BAD ONES
-"""
-import wget
-from bs4 import BeautifulSoup
-url = "https://www.facebook.com/hellomeets/events"
-
-down = wget.download(url)
-
-f = open(down, 'r')
-htmlText = "\n".join(f.readlines())
-f.close()
-print htmlText
-
-
-
-
-
-
-This is an example using pythons built in urllib2:
-
-import urllib2
-from bs4 import BeautifulSoup
-url = "https://www.facebook.com/hellomeets/events"
-
-html = urllib2.urlopen(url).read()
-print html
-"""
-
-
 
 # Use cron with wget
 # http://www.scrounge.org/linux/cron.html 
