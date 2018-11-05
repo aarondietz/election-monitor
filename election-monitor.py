b8f53a95a8011cd8cc7c70ac1ceeea58ec3fbf14
Aaron Dietz <aarondietz@users.noreply.github.com>
2018-11-04 19:52:14 -0800

    Top 10 Close and Solid races working
    But we'll need to update on the day of, when election results start coming in--not all results pages were up yet.

--- election-monitor.py
+++ election-monitor.py
@@ -23,7 +23,7 @@ exist.
 """
 
 # Import libraries
-# from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from datetime import datetime
 from pytz import timezone
 import os.path
@@ -31,6 +31,108 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 # from apscheduler.schedulers.background import BackgroundScheduler
 import re
 from urllib.request import Request, urlopen
+#import urllib.request
+import io
+
+
+# Set what web pages to get
+# Future functionality should ask what page to get, save a modifiable list of URLs
+urls = [
+        # GA 06: Lucy McBath vs Karen Handel - Needs updating
+        "http://sos.ga.gov/index.php/elections/daily_turnout_reports_for_november_6_2018_election", 
+        # TN Senate: Phil Bredesen vs Marsha Blackburn -  Might work
+        "https://www.elections.tn.gov/county-results.php?OfficeByCounty=United%20States%20Senate", 
+        # CA 49: Mike Levin vs. Diane Harkey - Needs updating
+        "https://www.sos.ca.gov/elections/prior-elections/statewide-election-results/", 
+        # CA 48: Harley Rouda vs. Dana Rohrabacker
+        # CA 22: Andrew Jantz vs. Devin Nunes
+        # CA 1: Audrey4congress vs. Doug LaMalfa
+        # TX Senate: Beto O'Rourke vs. Ted Cruz - needs updating
+        "https://www.sos.state.tx.us/elections/historical/index.shtml", 
+        # OH 12: Danny O'Connor vs. Troy Balderson - needs updating
+        "https://www.sos.state.oh.us/elections/election-results-and-data/2018-official-elections-results/", 
+        # FL Senate: Bill Nelson vs. Gov. Rick Scott- needs updating
+        "https://dos.myflorida.com/elections/data-statistics/elections-data/election-results-archive/",
+        # WI 01: IronStache vs. Bryan Steill - needs updating
+        "https://elections.wi.gov/elections-voting/results",
+        # KS 03: Sharice Davids vs. Kevin Yoder - could work but would prefer a source link, this is local media
+        "https://www.kwch.com/elections/?configID=1339",
+        
+        # ALSO TRACKING THE FOLLOWING 'SURE THING'S':
+        # UT Senate: Jenny Wilson vs. Mitt Romney 
+        "https://electionresults.utah.gov/elections/federal",
+        # MI 4th: John Moolenaar (R) vs. Jerry Hilliard (D) 
+        "https://www.michigan.gov/sos/0,4670,7-127-1633_8722---,00.html",
+        # PA 9th: Dan Meuser (R) vs. Denny Wolff 
+        "https://www.electionreturns.pa.gov/General/CountyResults?countyName=Schuylkill&ElectionID=63&ElectionType=G&IsActive=1",
+        # NC 11th: Mark Meadows vs. Phillip G. Price vs. Clifton Ingram Jr.
+        "https://www.ncsbe.gov/election-results",
+        # NY 26th: Brian Higgins (D) vs. Renee Zeno
+        "https://www.elections.ny.gov/2018ElectionResults.html",
+        # CT 3rd: Rosa L. DeLauro (D) vs. Angel Cadena
+        "https://portal.ct.gov/SOTS/Election-Services/Election-Results/Election-Results",
+        # OR 1st: Suzanne Bonamici vs. John Verbeek vs. Drew Layda
+        "https://sos.oregon.gov/elections/Pages/electionhistory.aspx",
+        # NM Senate: Martin Heinrich vs. Mick Rich vs. Gary Johnson
+        "http://electionresults.sos.state.nm.us/resultsSW.aspx?type=FED&map=CTY",
+        # MS Senate: David Baria (D) vs. Roger F. Wicker (R)
+        "http://www.sos.ms.gov/Elections-Voting/Pages/2018-Elections-Results.aspx",   
+        # WA Senate: Maria Cantwell vs. Susan Hutchinson 
+        "https://results.vote.wa.gov/results/current/Federal-All.html",
+        ]
+
+
+
+"""
+["http://example.com", 
+        "http://www.behindthename.com/random/"]
+"""
+# ---- More elections we could add, non national, based on Jennifer Cohn's elections to watch: 
+# Tony Evers vs. Scott Walker, WI governor
+# Andrew Gillum vs. Ron DeSantis for FL governor
+# Senator Laura Kelly vs. Kris Kobach for Kansas governor
+# Stacey Abrams vs. Brian Kemp for GA governor
+# Gretchen Whitmer vs. Bill Schuette for MI governor
+# Rich Cordray vs. Mike DeWine for OH governor
+# Kathleen Clyde vs. Frank LaRose for OH Sec of State
+
+# Also would be good to add solid elections--ones not expected to be a surprise, either way.
+
+
+
+
+
+# Set where to store the file/s, while ensuring directory/file name will 
+# be compatible with Windows
+save_here = 'C:/monitor/' 
+# Future functionality should ask where to save to
+
+
+# Set the retrieval frequency (in minutes) / how often 
+# to retrieve a copy of the site
+# Often I bump this down to 0.3 for testing, but probably should be at 5.0 for election day, assuming 144 collection times (set below)
+frequency = 1.0
+# Future functionality should ask the frequency
+
+
+# Set how long the program will run for.
+# Run the program this number of times / save this number of copies and then quit
+number_of_copies = 144
+
+
+# Initialize variable to keep track of how many times its run/how many copies saved
+copies_saved = 0
+
+
+# Establish what we'll call the saved version of the file
+# Future functionality should just grab the web page title or other info and use that
+# with remediation for duplicates (so if we've already grabbed a page titled 
+# that, it'll add a version number or do some other remediation)
+filename = "election_result"
+
+
+
+
 
 
 # Function where everything we need to repeat goes.
@@ -54,6 +156,12 @@ def repeat_tasks():
 def get_web_page(url_string):
     req = Request(url_string, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
+    
+    #get = urllib.request.urlopen(url_string)
+    #webpage = get.read()
+    #soup = BeautifulSoup(webpage, "lxml")
+    #return str(soup.encode("utf-8"))
+    
     mystr = webpage.decode("utf8")
     return mystr
 
@@ -118,114 +226,23 @@ def create_path(filename_string, save_path):
 # Got some of this code from: https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output
 def store_copy(page_to_save, path_to_save_to):  
     os.makedirs(os.path.dirname(path_to_save_to), exist_ok=True)
-    with open(path_to_save_to, "w") as file_obj:
+    with io.open(path_to_save_to, "w", encoding="utf-8") as file_obj:
+    
+    #with open(path_to_save_to, "w") as file_obj:
         file_obj.write(page_to_save)  # this is what is being written to the file
     file_obj.close()
 
 
-# Set what web pages to get
-# Future functionality should ask what page to get, save a modifiable list of URLs
-urls = [
-        # GA 06: Lucy McBath vs Karen Handel - Needs updating
-        "http://sos.ga.gov/index.php/elections/daily_turnout_reports_for_november_6_2018_election", 
-        # TN Senate: Phil Bredesen vs Marsha Blackburn -  Might work
-        "https://www.elections.tn.gov/county-results.php?OfficeByCounty=United%20States%20Senate", 
-        # CA 49: Mike Levin vs. Diane Harkey - Needs updating
-        "https://www.sos.ca.gov/elections/prior-elections/statewide-election-results/", 
-        # CA 48: Harley Rouda vs. Dana Rohrabacker
-        # CA 22: Andrew Jantz vs. Devin Nunes
-        # CA 1: Audrey4congress vs. Doug LaMalfa
-        # TX Senate: Beto O'Rourke vs. Ted Cruz - needs updating
-        "https://www.sos.state.tx.us/elections/historical/index.shtml", 
-        # OH 12: Danny O'Connor vs. Troy Balderson - needs updating
-        "https://www.sos.state.oh.us/elections/election-results-and-data/2018-official-elections-results/", 
-        # FL Senate: Bill Nelson vs. Gov. Rick Scott- needs updating
-        "https://dos.myflorida.com/elections/data-statistics/elections-data/election-results-archive/",
-        # WI 01: IronStache vs. Bryan Steill - needs updating
-        "https://elections.wi.gov/elections-voting/results",
-        # KS 03: Sharice Davids vs. Kevin Yoder - could work but would prefer a source link, this is local media
-        "https://www.kwch.com/elections/?configID=1339",
-        
-        # ALSO TRACKING THE FOLLOWING 'SURE THING'S':
-        # MS Senate: David Baria (D) vs. Roger F. Wicker (R)
-        "http://www.sos.ms.gov/Elections-Voting/Pages/2018-Elections-Results.aspx",   #start fixing how the program handles these links here
-        # UT Senate: Jenny Wilson vs. Mitt Romney 
-        "https://electionresults.utah.gov/elections/federal",
-        # WA Senate: Maria Cantwell vs. Susan Hutchinson 
-        "https://results.vote.wa.gov/results/current/Federal-All.html",
-        # NM Senate: Martin Heinrich vs. Mick Rich vs. Gary Johnson
-        "http://electionresults.sos.state.nm.us/resultsSW.aspx?type=FED&map=CTY",
-        # MI 4th: John Moolenaar (R) vs. Jerry Hilliard (D) 
-        "https://www.michigan.gov/sos/0,4670,7-127-1633_8722---,00.html",
-        # PA 9th: Dan Meuser (R) vs. Denny Wolff 
-        "https://www.electionreturns.pa.gov/General/CountyResults?countyName=Schuylkill&ElectionID=63&ElectionType=G&IsActive=1",
-        # NC 11th: Mark Meadows vs. Phillip G. Price vs. Clifton Ingram Jr.
-        "https://www.ncsbe.gov/election-results",
-        # OR 1st: Suzanne Bonamici vs. John Verbeek vs. Drew Layda
-        "https://sos.oregon.gov/elections/Pages/electionhistory.aspx",
-        # NY 26th: Brian Higgins (D) vs. Renee Zeno
-        "https://www.elections.ny.gov/2018ElectionResults.html",
-        # CT 3rd: Rosa L. DeLauro (D) vs. Angel Cadena
-        "https://portal.ct.gov/SOTS/Election-Services/Election-Results/Election-Results",
-        # last test
-        "http://example.com"
-        ]
-
-
-
-"""
-["http://example.com", 
-        "http://www.behindthename.com/random/"]
-"""
-# ---- More elections we could add, non national, based on Jennifer Cohn's elections to watch: 
-# Tony Evers vs. Scott Walker, WI governor
-# Andrew Gillum vs. Ron DeSantis for FL governor
-# Senator Laura Kelly vs. Kris Kobach for Kansas governor
-# Stacey Abrams vs. Brian Kemp for GA governor
-# Gretchen Whitmer vs. Bill Schuette for MI governor
-# Rich Cordray vs. Mike DeWine for OH governor
-# Kathleen Clyde vs. Frank LaRose for OH Sec of State
-
-# Also would be good to add solid elections--ones not expected to be a surprise, either way.
 
 
 # Initialize list for page titles, to be used as folder names to group downloaded results
 page_titles = []
+page_order = 1
 for x in urls:
-    page_titles.append(re.sub("[\.\t\,\:;\(\)\.\|]", "", get_url_title(x), 0, 0))
-    print(get_url_title(x))
-
-
-
-
-
-# Set where to store the file/s, while ensuring directory/file name will 
-# be compatible with Windows
-save_here = 'C:/monitor/' 
-# Future functionality should ask where to save to
-
-
-# Set the retrieval frequency (in minutes) / how often 
-# to retrieve a copy of the site
-# Often I bump this down to 0.3 for testing, but probably should be at 5.0 for election day, assuming 144 collection times (set below)
-frequency = 5.0
-# Future functionality should ask the frequency
-
-
-# Set how long the program will run for.
-# Run the program this number of times / save this number of copies and then quit
-number_of_copies = 144
-
-
-# Initialize variable to keep track of how many times its run/how many copies saved
-copies_saved = 0
-
-
-# Establish what we'll call the saved version of the file
-# Future functionality should just grab the web page title or other info and use that
-# with remediation for duplicates (so if we've already grabbed a page titled 
-# that, it'll add a version number or do some other remediation)
-filename = "election_result"
+    page_titles.append(re.sub('[^a-zA-Z0-9 \n\.]', '', str(page_order) + " " + get_url_title(x), 0, 0))
+    page_order += 1
+    # page_titles.append(re.sub("[\.\t\,\:;\(\)\.\|]", "", get_url_title(x), 0, 0))
+    print(page_titles[-1])
 
 
 
